8f44a2620fa5287cb46d66f14d1876761ff503f7
triciahughes <hughes.a.tricia@gmail.com>
2023-01-10 22:17:58 -0800

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const drivers = ["Antonia", "Nuru", "Amari", "Mo"];
+
+const returnFirstTwoDrivers = () => {
+  return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = () => {
+  return drivers.slice(2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = (fare) =>
+  function fareMultiplier() {
+    return fare * 5;
+  };
+
+const fareDoubler = (fare) => {
+  return fare * 2;
+};
+
+const fareTripler = (fare) => {
+  return fare * 3;
+};
+
+const selectDifferentDrivers = (drivers, returnFirstTwoDrivers) => {
+  return returnFirstTwoDrivers();
+};
